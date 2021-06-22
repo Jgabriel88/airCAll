9cd49d87fbf9735083ff33b323d42ed98e856ed7
Gabriel Silva <j.gabriel.888@gmail.com>
2021-06-21 21:56:18 -0400

    add styles to call item

--- src/components/CallsListItem.jsx
+++ src/components/CallsListItem.jsx
@@ -1,20 +1,23 @@
+import { Fragment } from 'react';
 import '../css/body.css';
 
 const CallsListItem = (props) => {
-	console.log('PROPS', props);
+	console.log('PROPS', props.create);
 	return (
-		<div className={'callItem'}>
-			{props.key}
-			{props.type}
-			{props.createdated_at}
-			{props.direction}
-			{props.duration}
-			{props.from}
-			{props.id}
-			{!props.isArchived && props.isArchived}
-			{props.to}
-			{props.via}
-		</div>
+		<>
+			<span className={'createdDate'}>{props.created}</span>
+			<div className={'callItem'}>
+				{props.key}
+				{props.type}
+				{props.direction}
+				{props.duration}
+				{props.from}
+				{props.id}
+				{!props.isArchived && props.isArchived}
+				{props.to}
+				{props.via}
+			</div>
+		</>
 	);
 };
 
