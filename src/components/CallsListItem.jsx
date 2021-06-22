693597d70ce25a3d13fe1125216d1e3980caddf8
Gabriel Silva <j.gabriel.888@gmail.com>
2021-06-21 22:27:20 -0400

    style with bootstrap on callitem

--- src/components/CallsListItem.jsx
+++ src/components/CallsListItem.jsx
@@ -1,24 +1,38 @@
-import { Fragment } from 'react';
 import '../css/body.css';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CallsListItem = (props) => {
 	console.log('PROPS', props.create);
 	return (
-		<>
-			<span className={'createdDate'}>{props.created}</span>
-			<div className={'callItem'}>
-				{props.key}
-				{props.type}
-				{props.direction}
-				{props.duration}
-				{props.from}
-				{props.id}
-				{!props.isArchived && props.isArchived}
-				{props.to}
-				{props.via}
-			</div>
-		</>
+		<div>
+			<span>{props.created}</span>
+			<Accordion defaultActiveKey={props.key} className={'mb-3'}>
+				<Card>
+					<Card.Header>{props.from}</Card.Header>
+					<Accordion.Collapse eventKey={props.key}>
+						<Card.Body>tried to call on {props.to}</Card.Body>
+					</Accordion.Collapse>
+				</Card>
+			</Accordion>
+		</div>
 	);
 };
 
 export default CallsListItem;
+
+// <Accordion>
+// 	<span className={'createdDate'}>{props.created}</span>
+// 	<div className={'callItem'}>
+// 		{props.key}
+// 		{props.type}
+// 		{props.direction}
+// 		{props.duration}
+// 		{props.from}
+// 		{props.id}
+// 		{!props.isArchived && props.isArchived}
+// 		{props.to}
+// 		{props.via}
+// 	</div>
+// <Accordion/>
